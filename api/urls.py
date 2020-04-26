c41243c28455d6332b682ebbfa2a0428882e5b2d
Nelson <nhiguera@vigigo.com>
2020-04-25 20:58:07 -0400

    Cambiado path

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'burguers', views.BurguerViewSet)
-router.register(r'ingredients', views.IngredientViewSet)
+router.register(r'hamburguesa', views.BurguerViewSet)
+router.register(r'ingrediente', views.IngredientViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
