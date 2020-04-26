6e98db5877614eef54caa21dd0d454876db4dc28
Nelson <nhiguera@vigigo.com>
2020-04-25 21:27:07 -0400

    Añadidos metodos para remover hamburguesas y ingredientes

--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 
 from api.models import Ingredient, Burguer
 
-class BurguerSerializer(serializers.HyperlinkedModelSerializer):
+class BurguerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Burguer
         fields = ('id_burguer', 'name', 'description', 'price', 'image', 'ingredients')
 
-class IngredientSerializer(serializers.HyperlinkedModelSerializer):
+class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ('id_ingredient', 'name', 'description')
--- api/views.py
+++ api/views.py
@@ -10,6 +10,29 @@ class BurguerViewSet(viewsets.ModelViewSet):
     queryset = Burguer.objects.all().order_by('name')
     serializer_class = BurguerSerializer
 
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, id=None):
+        burguer = Burguer.objects.get(id_burguer=id)
+
+    def destroy(self, request, pk=None):
+        burguer = Burguer.objects.get(id_burguer=pk)
+        burguer.delete()
+
+
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all().order_by('name')
     serializer_class = IngredientSerializer
+
+    def update(self, request, id=None):
+        pass
+
+    def partial_update(self, request, id=None):
+        ingredient = Ingredient.objects.get(id_ingredient=id)
+
+    def destroy(self, request, pk=None):
+        ingredient = Ingredient.objects.get(id_ingredient=pk)
+        burguers = ingredient.burguer_set.all()
+        if not burguers:
+            ingredient.delete()
